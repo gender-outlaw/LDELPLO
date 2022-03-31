c638ad2eb29db81915a3e665cb3a17b51a14278a
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-03-31 17:47:14 -0400

    root to public

--- webpack/prod.js
+++ webpack/prod.js
@@ -6,7 +6,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = merge(base, {
   mode: "production",
   output: {
-    filename: "bundle.min.js"
+    filename: "./public/main.js"
   },
   devtool: false,
   performance: {
